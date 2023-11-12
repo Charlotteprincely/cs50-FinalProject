36e0bdb73a881481a1c9ad4917dafc4b03a463a0
Mohammad Ali Kazemi <mak.iran.96@gmail.com>
2023-11-12 10:58:22 +0330

    21 aban

--- lib/blog_page.dart
+++ lib/blog_page.dart
@@ -17,51 +17,145 @@ class BlogScreen extends StatelessWidget {
       ),
       body: SafeArea(
         child: Center(
-          child: Padding(
-            padding: EdgeInsets.all(15),
-            child: Column(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: Image(
-                    image: AssetImage('images/a.png'),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image(
+                      image: AssetImage('images/a.png'),
+                    ),
                   ),
-                ),
-                SizedBox(height: 10),
-                Text('Alchemy Pay سیگنال خرید: برای 20 آبان'),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'فروش: 14,250',
-                      style: TextStyle(color: Colors.red),
-                    ),
-                    SizedBox(width: 3),
-                    Icon(
-                      Icons.monetization_on,
-                      color: Colors.red,
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      'خرید: 13,500',
-                      style: TextStyle(color: Colors.green),
-                    ),
-                    SizedBox(width: 3),
-                    Icon(
-                      Icons.monetization_on,
-                      color: Colors.green,
-                    ),
-                  ],
-                ),
-                Container(
-                  width: 150,
-                  child: Divider(
-                    thickness: 2,
-                    color: Colors.black,
+                  SizedBox(height: 10),
+                  Text('Alchemy Pay سیگنال خرید: برای 20 آبان'),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'فروش: 14,250',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                      SizedBox(width: 3),
+                      Icon(
+                        Icons.monetization_on,
+                        color: Colors.red,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'خرید: 13,500',
+                        style: TextStyle(color: Colors.green),
+                      ),
+                      SizedBox(width: 3),
+                      Icon(
+                        Icons.monetization_on,
+                        color: Colors.green,
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                  Container(
+                    width: 150,
+                    child: Divider(
+                      thickness: 2,
+                      color: Colors.black,
+                    ),
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image(
+                      image: AssetImage('images/c.png'),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text('Alchemy Pay سیگنال خرید: برای 20 آبان'),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'فروش: 14,250',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                      SizedBox(width: 3),
+                      Icon(
+                        Icons.monetization_on,
+                        color: Colors.red,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'خرید: 13,500',
+                        style: TextStyle(color: Colors.green),
+                      ),
+                      SizedBox(width: 3),
+                      Icon(
+                        Icons.monetization_on,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 150,
+                    child: Divider(
+                      thickness: 2,
+                      color: Colors.black,
+                    ),
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image(
+                      image: AssetImage('images/r.png'),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text('Alchemy Pay سیگنال خرید: برای 20 آبان'),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'فروش: 14,250',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                      SizedBox(width: 3),
+                      Icon(
+                        Icons.monetization_on,
+                        color: Colors.red,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'خرید: 13,500',
+                        style: TextStyle(color: Colors.green),
+                      ),
+                      SizedBox(width: 3),
+                      Icon(
+                        Icons.monetization_on,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 150,
+                    child: Divider(
+                      thickness: 2,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextButton(
+                    style: TextButton.styleFrom(foregroundColor: Colors.red),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'خروج از حساب',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
